47e68800e5831047f97279587dd5c017328bd23b
James Brind <jb753@cam.ac.uk>
2019-11-15 15:09:07 +0000

    Tests now in pytest structure

--- test/test_compflow.py
+++ test/test_compflow.py
@@ -1,47 +1,95 @@
 import compflow as cf
 import numpy as np
 
-
-# Load validation data from CUED Thermofluids Data Book
+# Load validation data
 ga = 1.4
 # Subsonic
 var_sub = ['Ma', 'To_T', 'Po_P', 'rhoo_rho', 'V_cpTo', 'mcpTo_APo', 'mcpTo_AP', 'F_mcpTo', '4cfL_D', 'rhoVsq_2Po']
-dat_sub = {var_sub[i]: np.loadtxt('compflow/test/cued_data_book_subsonic.csv').reshape((-1, 10))[1:, i]
+dat_sub = {var_sub[i]: np.loadtxt('test/cued_data_book_subsonic.csv').reshape((-1, 10))[:, i]
            for i in range(len(var_sub))}
 # Supersonic
 var_sup = var_sub + ['Mash', 'Posh_Po', 'Psh_P', 'Posh_P', 'Tsh_T', 'nu', 'Ma2']
-dat_sup = {var_sup[i]: np.loadtxt('compflow/test/cued_data_book_supersonic.csv').reshape((-1, 17))[1:, i]
+dat_sup = {var_sup[i]: np.loadtxt('test/cued_data_book_supersonic.csv').reshape((-1, 17))[3:, i]
            for i in range(len(var_sup))}
 
 # Take reciprocals
-for v in ['To_T', 'Po_P', 'rhoo_rho']:
-    dat_sub[v] = 1. / dat_sub[v]
-    dat_sup[v] = 1. / dat_sup[v]
+for vi in ['To_T', 'Po_P', 'rhoo_rho']:
+    dat_sub[vi] = 1. / dat_sub[vi]
+    dat_sup[vi] = 1. / dat_sup[vi]
 
 # All variables which the module implements
 var_test_sub = ['To_T', 'Po_P', 'rhoo_rho', 'V_cpTo', 'mcpTo_APo', 'mcpTo_AP']
 var_test_sup = ['To_T', 'Po_P', 'rhoo_rho', 'V_cpTo', 'mcpTo_APo', 'mcpTo_AP', 'Mash', 'Posh_Po']
 
-# Subsonic tests
-for v in var_test_sub:
-    #
-    # Error w.r.t. table looking up Ma
-    err_forward = np.abs(np.round(cf.from_Ma(v, dat_sub['Ma'], ga), 4) - dat_sub[v])
-    assert np.max(err_forward) < 2.e-4
 
-    # Error w.r.t. table finding Ma
-    err_back = np.abs(cf.to_Ma(v, dat_sub[v], ga) - dat_sub['Ma'])
-    assert np.max(err_back) < 5.e-3
+#
+# Begin test functions
 
-# Supersonic tests
-for v in var_test_sup:
-    #
-    # Error w.r.t. table looking up Ma
-    err_forward = np.abs(cf.from_Ma(v, dat_sup['Ma'], ga) / dat_sup[v] - 1.)
-    assert np.max(err_forward) < 0.005
+# Check corner cases
+def test_Ma_0():
+    # Expected values for Ma=0
+    val0 = np.array([1., 1., 1., 0., 0., 0., np.nan, np.nan])
+    Y0 = {var_test_sup[i]: val0[i] for i in range(len(var_test_sup))}
+    for v in var_test_sup:
+        # Ma = 0
+        assert np.isclose(cf.from_Ma(v, 0., ga), Y0[v], atol=1e-7, equal_nan=True)
+        if np.isfinite(Y0[v]):
+            assert np.isclose(cf.to_Ma(v, Y0[v], ga), 0.0, atol=1e-7, equal_nan=True)
 
-    # Error w.r.t. table finding Ma
-    err_back = np.abs(cf.to_Ma(v, dat_sup[v], ga, supersonic=True) - dat_sup['Ma'])
-    assert np.sqrt(np.mean(err_back**2.) < 0.001)
-    assert np.max(err_back) < 0.05
 
+# Compare code output to CUED Thermofluids Data Book
+def test_explicit_sub():
+    for v in var_test_sub:
+        Ysub = cf.from_Ma(v, dat_sub['Ma'], ga)
+        err_sub = np.abs(Ysub / dat_sub[v] - 1.)
+        imax = np.argmax(err_sub)
+        assert err_sub[imax] < 0.005, \
+            "Ma={0} => {1}={2} with error={3}".format(
+                dat_sub['Ma'][imax], v, Ysub[imax], err_sub[imax])
+
+
+def test_explicit_sup():
+    for v in var_test_sup:
+        Ysup = cf.from_Ma(v, dat_sup['Ma'], ga)
+        err_sup = np.abs(Ysup / dat_sup[v] - 1.)
+        imax = np.argmax(err_sup)
+        assert err_sup[imax] < 0.005, \
+            "Ma={0} => {1}={2} with error={3}".format(
+                dat_sup['Ma'][imax], v, Ysup[imax], err_sup[imax])
+
+
+def test_inverse_sub():
+    for v in var_test_sub:
+        X = cf.to_Ma(v, dat_sub[v], ga)
+        err_sub = np.abs(X - dat_sub['Ma'])
+        imax = np.argmax(err_sub)
+        assert err_sub[imax] < 0.01, \
+            "{0}={1} =>  Ma={2} with error={3}".format(
+                v, dat_sub[v][imax], X[imax], err_sub[imax])
+
+
+def test_inverse_sup():
+    for v in var_test_sup:
+        X = cf.to_Ma(v, dat_sup[v], ga, supersonic=True)
+        err_sup = np.abs(X - dat_sup['Ma'])
+        imax = np.argmax(err_sup)
+        assert err_sup[imax] < 0.01, \
+            "{0}={1} =>  Ma={2} with error={3}".format(
+                v, dat_sup[v][imax], X[imax], err_sup[imax])
+
+
+# Check explicit derivative against a numerical approximation
+def test_derivative():
+    for v in var_test_sup:
+        Ma = np.linspace(0., 3.)
+
+        # Explicit
+        dYdMa = cf.derivative_from_Ma(v, Ma, ga)
+
+        # Numerical approximation
+        Y = cf.from_Ma(v, Ma, ga)
+        dYdMa_numerical = np.gradient(Y, Ma)
+
+        # Check discrepancy
+        err = dYdMa / dYdMa_numerical - 1.
+        assert np.max(err[np.isfinite(err)]) < 0.05
