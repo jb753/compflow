76719c2c680b0e4f72bd454856ead19cd4738d31
James Brind <jb753@cam.ac.uk>
2020-12-21 16:54:50 +0000

    Finish first implementation of Fortran

--- run_bench.py
+++ run_bench.py
@@ -1,15 +1,15 @@
 import numpy as np
 import timeit
 # from numba import njit
-from compflow_fort import a_acrit as func_fort
-from compflow import Ma_from_A_Acrit as func_pure
+from compflow_fort import posh as func_fort
+from compflow import Ma_from_Posh_Po as func_pure
 
 if __name__ == '__main__':
 
     N1 = 10
     N2 = 100
     Ma = np.linspace(0.1,1.4,N1)
-    Ma = np.array([1.1])
+    # Ma = np.array([0.6])
     ga = 1.4
 
     print(func_fort(Ma,ga))
--- to_Ma.f
+++ to_Ma.f
@@ -286,3 +286,172 @@ Cf2py intent(hide) n
         ENDIF
       ENDDO
       END
+
+      SUBROUTINE POSH(M,X,G,N)
+      INTEGER N
+      INTEGER K
+      REAL*8 G
+      REAL*8 M(N)
+      REAL*8 X(N)
+Cf2py intent(in) x
+Cf2py intent(in) g
+Cf2py intent(out) m
+Cf2py intent(hide) n
+      REAL*8 GM1
+      REAL*8 GP1
+      REAL*8 GM1_GP1
+      REAL*8 G_SQ_GM1
+      REAL*8 GM1_2
+      REAL*8 GP1_2
+      REAL*8 G_GM1
+      REAL*8 MG_GM1
+      REAL*8 M_GM1
+      REAL*8 P_GM1
+      REAL*8 MA
+      REAL*8 MANEW
+      REAL*8 ERR
+      REAL*8 F
+      REAL*8 DF
+
+
+      REAL*8 NAN
+      REAL*8 TOL
+
+      REAL*8 A
+      REAL*8 B
+      REAL*8 C
+
+
+      GM1 = G-1.0D0
+      GP1 = G+1.0D0
+      GM1_2 = GM1/2.0D0
+      GP1_2 = GP1/2.0D0
+      GM1_GP1 = GM1/GP1
+      G_SQ_GM1 = G/SQRT(GM1)
+      G_GM1 = G/GM1
+      MG_GM1 = -1.0D0*G/GM1
+      M_GM1 = -1.0D0/GM1
+      P_GM1 = 1.0D0/GM1
+
+      NAN = 0.0D0
+      NAN = 0.0D0/NAN
+      TOL = 1.0D-6
+
+      DO I=1,N
+        IF (X(I).gt.1.0D0) THEN
+            M(I) = NAN
+        ELSE
+        MA = 1.5D0
+        K = 0
+        ERR = HUGE(ERR)
+        DO WHILE (ERR.gt.TOL.and.K.lt.100)
+
+        K = K + 1
+
+        A = GP1_2*MA*MA/(1.0D0 + GM1_2 * MA * MA)
+        B = G/GP1_2*MA*MA - GM1_GP1
+
+        F = A**G_GM1 * B**M_GM1
+
+        A = (G*MA*((MA*MA-1.0D0)/(1.0D0+GM1_2*MA*MA))**2.0D0)
+        B = (GP1_2*MA*MA/(1.0D0+GM1_2*MA*MA))
+        C = (G/GP1_2*MA*MA-GM1_GP1)
+
+        DF = -1.0D0*(G*MA*((MA*MA-1.0D0)/(1.0D0+GM1_2*MA*MA))**2.0D0) 
+     &   *((GP1_2*MA*MA/(1.0D0+GM1_2*MA*MA))**P_GM1)
+     &   *((G/GP1_2*MA*MA-GM1_GP1)**MG_GM1)
+
+        MANEW = MA - (F-X(I)) / DF
+
+        ERR = ABS(MANEW - MA)
+        MA = MANEW
+
+
+        END DO
+
+        IF (ERR.lt.TOL) THEN
+            M(I) = MA
+        ELSE
+            M(I) = NAN
+        ENDIF
+        ENDIF
+      ENDDO
+      END
+
+      SUBROUTINE MASH(M,X,G,N)
+      INTEGER N
+      INTEGER K
+      REAL*8 G
+      REAL*8 M(N)
+      REAL*8 X(N)
+Cf2py intent(in) x
+Cf2py intent(in) g
+Cf2py intent(out) m
+Cf2py intent(hide) n
+      REAL*8 GM1
+      REAL*8 GP1
+      REAL*8 GP1_GM1_2
+      REAL*8 G_SQ_GM1
+      REAL*8 GM1_2
+      REAL*8 GP1_2
+      REAL*8 MA
+      REAL*8 MANEW
+      REAL*8 ERR
+      REAL*8 F
+      REAL*8 DF
+
+
+      REAL*8 NAN
+      REAL*8 TOL
+
+      REAL*8 A
+      REAL*8 B
+      REAL*8 C
+
+
+      GM1 = G-1.0D0
+      GM1_2 = (G-1.0D0)/2.0D0
+      GP1_2 = (G+1.0D0)/2.0D0
+      GP1 = G+1.0D0
+      GP1_GM1_2 = GP1/GM1/(2.0D0)
+      G_SQ_GM1 = G/SQRT(GM1)
+
+      NAN = 0.0D0
+      NAN = 0.0D0/NAN
+      TOL = 1.0D-6
+
+      DO I=1,N
+        IF (X(I).gt.1.0D0) THEN
+            M(I) = NAN
+        ELSE
+        MA = 0.5D0
+        K = 0
+        ERR = HUGE(ERR)
+        DO WHILE (ERR.gt.TOL.and.K.lt.100)
+
+        K = K + 1
+
+        F = SQRT((1.0D0 + GM1_2*MA*MA)/(G*MA*MA - GM1_2))
+
+        A = GP1**2.0D0 * MA / SQRT(2.0D0)
+        B = -1.0D0/SQRT(1.0D0 + GM1_2 * MA * MA)
+        C = G * (2.0D0*MA*MA - 1.0D0) + 1.0D0
+
+        DF = A * B / C / SQRT(C)
+
+        MANEW = MA - (F-X(I)) / DF
+
+        ERR = ABS(MANEW - MA)
+        MA = MANEW
+
+
+        END DO
+
+        IF (ERR.lt.TOL) THEN
+            M(I) = MA
+        ELSE
+            M(I) = NAN
+        ENDIF
+        ENDIF
+      ENDDO
+      END
