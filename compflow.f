4c940192bdc5676194ab705aa9991bd51570828d
James Brind <jb753@cam.ac.uk>
2020-12-21 11:41:44 +0000

    Work on Fortran

--- compflow.f
+++ compflow.f
@@ -1,5 +1,5 @@
       SUBROUTINE TO_T(Y,M,G,N)
-C	  1-(G-1)/2 * M^2
+C	  1-(g-1)/2 * M^2
       INTEGER N
       REAL*8 G
       REAL*8 M(N)
@@ -53,7 +53,7 @@ Cf2py intent(hide) n
       ENDDO
       END
 
-      SUBROUTINE V_CTPO(Y,M,G,N)
+      SUBROUTINE V_CPTO(Y,M,G,N)
 C	  (1-(G-1)/2 * M^2)^(1/(G-1))
 C    V_cpTo = np.sqrt(2.) * np.ones_like(Ma)  # Limit for Ma -> inf
 C    V_cpTo[ii] = np.sqrt( (ga - 1.0) * Ma[ii] ** 2. / (1. + (ga - 1.) * Ma[ii] **2. /2.) )
@@ -70,9 +70,58 @@ Cf2py intent(hide) n
       GM1_2 = (G-1.0D0)*0.5D0
       GM1 = G-1.0D0
       DO I=1,N
-            Y(I)=SQRT(GM1*M(I)*M(I)/(1.0D0+GM1_2*M(I)*M(I)))
+            IF (M(I).lt.1.0D-2) THEN
+                Y(I)=0.0D0
+            ELSE
+                Y(I)=SQRT(GM1/(1.0D0/M(I)/M(I)+GM1_2))
+            ENDIF
       ENDDO
       END
+
+
+      SUBROUTINE MCPTO_APO(Y,M,G,N)
+      INTEGER N
+      REAL*8 G
+      REAL*8 M(N)
+      REAL*8 Y(N)
+Cf2py intent(in) m
+Cf2py intent(in) g
+Cf2py intent(out) y
+Cf2py intent(hide) n
+      REAL*8 M_GP1_GM1_2
+      REAL*8 GM1_2
+      REAL*8 G_SQ_GM1
+      GM1_2 = (G-1.0D0)/2.0D0
+      M_GP1_GM1_2 = (G+1.0D0)/(G-1.0D0)/(-2.0D0)
+      G_SQ_GM1 = G / SQRT(G-1.0D0)
+      DO I=1,N
+        Y(I)=G_SQ_GM1*M(I)*(1.0D0+GM1_2*M(I)*M(I))**M_GP1_GM1_2
+      ENDDO
+      END
+
+
+      SUBROUTINE MCPTO_AP(Y,M,G,N)
+      INTEGER N
+      REAL*8 G
+      REAL*8 M(N)
+      REAL*8 Y(N)
+Cf2py intent(in) m
+Cf2py intent(in) g
+Cf2py intent(out) y
+Cf2py intent(hide) n
+      REAL*8 M_GP1_GM1_2
+      REAL*8 GM1_2
+      REAL*8 G_SQ_GM1
+      GM1_2 = (G-1.0D0)/2.0D0
+      M_GP1_GM1_2 = (G+1.0D0)/(G-1.0D0)/(-2.0D0)
+      G_SQ_GM1 = G / SQRT(G-1.0D0)
+      DO I=1,N
+        Y(I)=G_SQ_GM1*M(I)*SQRT(1.0D0+GM1_2*M(I)*M(I))
+      ENDDO
+      END
+
+
+
 
 
 
--- run_bench.py
+++ run_bench.py
@@ -1,8 +1,8 @@
 import numpy as np
 import timeit
 # from numba import njit
-from compflow_fort import po_p as func_fort
-from compflow import V_cpTo_from_Ma as func_pure
+from compflow_fort import mcpto_apo as func_fort
+from compflow import mcpTo_APo_from_Ma as func_pure
 
 # def Po_P_pure(Ma,ga):
 # return (1. + 0.5*(ga - 1.)*Ma**2.)**(ga/(ga-1.0))
@@ -16,9 +16,10 @@ if __name__ == '__main__':
     N1 = 100
     N2 = 10000
     Ma = np.linspace(0,1,N1)
-    Ma = np.array([ 0.3 ])
     ga = 1.4
 
+    print(func_fort(Ma,ga))
+    print(func_pure(Ma,ga))
     # for _ in range(N2):
     #     Po_P_fort(Ma,ga)
 
