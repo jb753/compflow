efe35e7c3e55f91c4464573a2bba02219b35f3b9
James Brind <jb753@cam.ac.uk>
2020-12-21 15:32:19 +0000

    Critical area

--- run_bench.py
+++ run_bench.py
@@ -1,21 +1,19 @@
 import numpy as np
 import timeit
 # from numba import njit
-from compflow_fort import mcpto_apo as func_fort
-from compflow import Ma_from_mcpTo_APo as func_pure
+from compflow_fort import a_acrit as func_fort
+from compflow import Ma_from_A_Acrit as func_pure
 
 if __name__ == '__main__':
 
     N1 = 10
     N2 = 100
-    # Ma = np.linspace(0.1,1.4,N1)
+    Ma = np.linspace(0.1,1.4,N1)
     Ma = np.array([1.1])
     ga = 1.4
 
-    print(func_fort(Ma,ga,True))
-    print(func_fort(Ma,ga,False))
-    print(func_pure(Ma,ga,True))
-    print(func_pure(Ma,ga,False))
+    print(func_fort(Ma,ga))
+    print(func_pure(Ma,ga))
 
     # for _ in range(N2):
     #     Po_P_fort(Ma,ga)
--- to_Ma.f
+++ to_Ma.f
@@ -70,16 +70,14 @@ C    return np.sqrt( V_cpTo **2. / (ga - 1.) / (1. - 0.5 * V_cpTo **2.))
       ENDDO
       END
 
-      SUBROUTINE MCPTO_APO(M,X,G,SUP,N)
+      SUBROUTINE MCPTO_AP(M,X,G,N)
       INTEGER N
       INTEGER K
       REAL*8 G
       REAL*8 M(N)
       REAL*8 X(N)
-      LOGICAL SUP
 Cf2py intent(in) x
 Cf2py intent(in) g
-Cf2py optional,intent(in) :: SUP = .FALSE.
 Cf2py intent(out) m
 Cf2py intent(hide) n
       REAL*8 GM1
@@ -116,20 +114,16 @@ Cf2py intent(hide) n
         IF (X(I).gt.FCRIT) THEN
             M(I) = NAN
         ELSE
-            IF (SUP) THEN
-                MA = 1.5D0
-            ELSE
-                MA = 0.5D0
-            ENDIF
+            MA = 0.5D0
             K = 0
             ERR = HUGE(ERR)
             DO WHILE (ERR.gt.TOL.and.K.lt.100)
 
             K = K + 1
 
-            F = G_SQ_GM1 * MA * (1.0D0 + GM1_2 * MA*MA )**M_GP1_GM1_2
-            DF = G_SQ_GM1 * ( (1.0D0 + GM1_2 * MA * MA )**M_GP1_GM1_2 
-     &       - GP1_2 * MA * MA * (1.0D0 + GM1_2 * MA*MA)**M_GP1_GM1_2 )
+            F = G_SQ_GM1 * MA * SQRT(1.0D0 + GM1_2 * MA*MA )
+            DF = G_SQ_GM1 * ( (1.0D0 + GM1_2 * MA * MA )**2.0D0 
+     &       - GM1_2 * MA * MA / SQRT(1.0D0 + GM1_2 * MA*MA))
 
             MANEW = MA - (F-X(I)) / DF
 
@@ -147,3 +141,148 @@ Cf2py intent(hide) n
       ENDDO
       END
 
+
+      SUBROUTINE MCPTO_APO(M,X,G,SUP,N)
+      INTEGER N
+      INTEGER K
+      REAL*8 G
+      REAL*8 M(N)
+      REAL*8 X(N)
+      LOGICAL SUP
+Cf2py optional,intent(in) :: SUP = .FALSE.
+Cf2py intent(in) x
+Cf2py intent(in) g
+Cf2py intent(out) m
+Cf2py intent(hide) n
+      REAL*8 GM1
+      REAL*8 GP1
+      REAL*8 M_GP1_GM1_2
+      REAL*8 G_SQ_GM1
+      REAL*8 GM1_2
+      REAL*8 GP1_2
+      REAL*8 MA
+      REAL*8 MANEW
+      REAL*8 ERR
+      REAL*8 F
+      REAL*8 DF
+
+
+      REAL*8 NAN
+      REAL*8 TOL
+
+      GM1 = G-1.0D0
+      GM1_2 = (G-1.0D0)/2.0D0
+      GP1_2 = (G+1.0D0)/2.0D0
+      GP1 = G+1.0D0
+      M_GP1_GM1_2 = GP1/GM1/(-2.0D0)
+      G_SQ_GM1 = G/SQRT(GM1)
+
+      NAN = 0.0D0
+      NAN = 0.0D0/NAN
+      TOL = 1.0D-6
+
+
+      DO I=1,N
+        IF (SUP) THEN
+            MA = 1.5D0
+        ELSE
+            MA = 0.5D0
+        ENDIF
+        K = 0
+        ERR = HUGE(ERR)
+        DO WHILE (ERR.gt.TOL.and.K.lt.100)
+
+        K = K + 1
+
+        F = G_SQ_GM1 * MA * (1.0D0 + GM1_2 * MA*MA )**M_GP1_GM1_2
+        DF = G_SQ_GM1 * ( (1.0D0 + GM1_2 * MA * MA )**M_GP1_GM1_2 
+     &       - GP1_2 * MA * MA * (1.0D0 + GM1_2 * MA*MA)**M_GP1_GM1_2 )
+
+        MANEW = MA - (F-X(I)) / DF
+
+        ERR = ABS(MANEW - MA)
+        MA = MANEW
+
+        END DO
+
+        IF (ERR.lt.TOL) THEN
+            M(I) = MA
+        ELSE
+            M(I) = NAN
+        ENDIF
+      ENDDO
+      END
+
+
+
+
+      SUBROUTINE A_ACRIT(M,X,G,SUP,N)
+      INTEGER N
+      INTEGER K
+      REAL*8 G
+      REAL*8 M(N)
+      REAL*8 X(N)
+      LOGICAL SUP
+Cf2py optional,intent(in) :: SUP = .FALSE.
+Cf2py intent(in) x
+Cf2py intent(in) g
+Cf2py intent(out) m
+Cf2py intent(hide) n
+      REAL*8 GM1
+      REAL*8 GP1
+      REAL*8 GP1_GM1_2
+      REAL*8 G_SQ_GM1
+      REAL*8 GM1_2
+      REAL*8 GP1_2
+      REAL*8 MA
+      REAL*8 MANEW
+      REAL*8 ERR
+      REAL*8 F
+      REAL*8 DF
+
+
+      REAL*8 NAN
+      REAL*8 TOL
+
+      GM1 = G-1.0D0
+      GM1_2 = (G-1.0D0)/2.0D0
+      GP1_2 = (G+1.0D0)/2.0D0
+      GP1 = G+1.0D0
+      GP1_GM1_2 = GP1/GM1/(2.0D0)
+      G_SQ_GM1 = G/SQRT(GM1)
+
+      NAN = 0.0D0
+      NAN = 0.0D0/NAN
+      TOL = 1.0D-6
+
+
+      DO I=1,N
+        IF (SUP) THEN
+            MA = 1.5D0
+        ELSE
+            MA = 0.5D0
+        ENDIF
+        K = 0
+        ERR = HUGE(ERR)
+        DO WHILE (ERR.gt.TOL.and.K.lt.100)
+
+        K = K + 1
+
+        F = (((1.0D0 + GM1_2 * MA*MA )/GP1_2)**GP1_GM1_2)/MA
+        DF = ( (1.0D0 + GM1_2 * MA * MA)/GP1_2 )**GP1_GM1_2 
+     &       * -1.0D0/MA/MA + GP1_2 /(1.0D0 + GM1_2 * MA*MA)
+
+        MANEW = MA - (F-X(I)) / DF
+
+        ERR = ABS(MANEW - MA)
+        MA = MANEW
+
+        END DO
+
+        IF (ERR.lt.TOL) THEN
+            M(I) = MA
+        ELSE
+            M(I) = NAN
+        ENDIF
+      ENDDO
+      END
