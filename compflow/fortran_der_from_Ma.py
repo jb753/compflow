1bd4f49d39fb68cdacee9be4aef1e008314b5bc6
James Brind <jb753@cam.ac.uk>
2020-12-28 12:48:05 +0000

    Change structure

---
+++ compflow/fortran_der_from_Ma.py
@@ -0,0 +1,11 @@
+from .fort_der_from_Ma import *
+
+der_To_T_from_Ma = to_t
+der_Po_P_from_Ma = po_p
+der_rhoo_rho_from_Ma = rhoo_rho
+der_V_cpTo_from_Ma = v_cpto
+der_mcpTo_APo_from_Ma = mcpto_apo
+der_mcpTo_AP_from_Ma = mcpto_ap
+der_A_Acrit_from_Ma = a_acrit
+der_Mash_from_Ma = mash
+der_Posh_Po_from_Ma = posh_po
---
+++ compflow/fortran_from_Ma.py
@@ -0,0 +1,11 @@
+from .fort_from_Ma import *
+
+To_T_from_Ma = to_t
+Po_P_from_Ma = po_p
+rhoo_rho_from_Ma = rhoo_rho
+V_cpTo_from_Ma = v_cpto
+mcpTo_APo_from_Ma = mcpto_apo
+mcpTo_AP_from_Ma = mcpto_ap
+A_Acrit_from_Ma = a_acrit
+Mash_from_Ma = mash
+Posh_Po_from_Ma = posh_po
