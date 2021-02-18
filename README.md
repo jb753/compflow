7ede6cb860a2573a1ab5e7e40b1591c3e72c3783
James Brind <jb753@cam.ac.uk>
2021-02-18 15:19:02 +0000

    Tweak readme.

--- README.md
+++ README.md
@@ -97,6 +97,7 @@ returned,
 * Add test for invalid values of Mach number.
 * Add test for invalid values of inversion quantities.
 * Add test for compatibility of output shape.
+* Sort out packaging so that NumPy gets installed automatically.
 
 James Brind
 Feb 2021
--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 import setuptools
+
 from numpy.distutils.core import setup
 from numpy.distutils.core import Extension
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 ext = [Extension(name = 'compflow_fort_from_Ma', sources = ['fortran/from_Ma.f']),
        Extension(name = 'compflow_fort_der_from_Ma', sources = ['fortran/der_from_Ma.f']),
        Extension(name = 'compflow_fort_to_Ma', sources = ['fortran/to_Ma.f'])]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="compflow",
-    version="0.2.1",
+    version="0.3.0",
     author="James Brind",
     author_email="jb753@cam.ac.uk",
     description="Compressible aerodynamics library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jb753/compflow",
+    url="https://jb753.user.srcf.net/compflow-docs/index.html",
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy",
@@ -29,5 +30,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
-    ext_modules=ext
+    keywords=['compressible','flow','aerodynamics','fluid','engineering'],
+    ext_modules=ext,
 )
